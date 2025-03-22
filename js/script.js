6c82878f37f6d9ebc5d24eb10f2971b58f024722
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-03-23 00:57:24 +0400

    scrollbar thumb style

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 let changeMode = document.getElementsByClassName('mode')[0];
 let sunIcon = document.getElementsByClassName('fa-sun')[0];
+let body = document.querySelector('body')
 
 changeMode.addEventListener('click',() =>{
     if(sunIcon.classList.contains('fa-sun')){
-        sunIcon.classList.toggle('fa-moon')
+        sunIcon.classList.toggle('fa-moon');
     }
 })
 
