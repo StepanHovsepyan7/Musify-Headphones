9de419d6beefbc7c95fda0da4a72db16e7d6d81c
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-03-26 00:15:14 +0400

    dark and light mode for h1

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 let changeMode = document.getElementsByClassName('mode')[0];
 let sunIcon = document.getElementsByClassName('fa-sun')[0];
 let body = document.querySelector('body');
-let titles = document.querySelectorAll('h1')
+let titles = document.querySelectorAll('h1');
 
-changeMode.addEventListener('click',() =>{
-    if(sunIcon.classList.contains('fa-sun')){
-        sunIcon.classList.toggle('fa-moon');
-        body.classList.toggle('bodycolor');
+
+changeMode.addEventListener('click', () => {
+    if (sunIcon.classList.contains('fa-sun')) {
+        sunIcon.classList.remove('fa-sun');
+        sunIcon.classList.add('fa-moon');
+        body.classList.add('bodycolor');
         titles.forEach(title => {
             title.classList.remove('dark-mode');
             title.classList.add('lightMode');
         });
-    }else{
+    } else {
+        sunIcon.classList.remove('fa-moon');
+        sunIcon.classList.add('fa-sun');
+        body.classList.remove('bodycolor');
         titles.forEach(title => {
             title.classList.remove('lightMode');
             title.classList.add('dark-mode');
         });
     }
-})
-
-
+});
