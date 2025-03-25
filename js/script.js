f7909748f0213707a993ff3ebc034f5695c61330
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-03-25 23:43:36 +0400

    light mode with bugs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 let changeMode = document.getElementsByClassName('mode')[0];
 let sunIcon = document.getElementsByClassName('fa-sun')[0];
-let body = document.querySelector('body')
+let body = document.querySelector('body');
+let titles = document.querySelectorAll('h1')
 
 changeMode.addEventListener('click',() =>{
     if(sunIcon.classList.contains('fa-sun')){
         sunIcon.classList.toggle('fa-moon');
+        body.classList.toggle('bodycolor');
+        titles.forEach(title => {
+            title.classList.remove('dark-mode');
+            title.classList.add('lightMode');
+        });
+    }else{
+        titles.forEach(title => {
+            title.classList.remove('lightMode');
+            title.classList.add('dark-mode');
+        });
     }
 })
 
